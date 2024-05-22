50349f6ad44d1052aaed36fc7df3da85f5d38d3b
adiazma <u201415388@upc.edu.pe>
2024-05-22 18:24:26 -0500

    Update prompts

--- config.py
+++ config.py
@@ -4,34 +4,29 @@ import json, time
 
 total = []
 client = OpenAI()
-df = pd.read_excel('datos_prueba.xlsx')
-for item in df.to_dict('records'):
+for item in json.loads(open('response_prompt.json').read()):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": """
-            Del siguiente json {json_inicio} dame la clasificación de morosidad de la persona en cuestión. Dime SOLAMENTE si es "ALTO", "MEDIO", "BAJO" segun tus criterios
-            y la data compartida.
+            Del siguiente json {json_inicio} dame la clasificación de morosidad para la empresa Telefonica en el siguiente formato {json_fin} de la persona en cuestión. 
+            SI NO se puede determinar a partir de la información proporcionada, APROXIMALA PARA TODOS LOS CAMPOS SEGUN LOS DATOS QUE TENGAS DE PERÚ. 
         """.format(
             json_inicio=json.dumps(item),
             json_fin=json.dumps({
-                'Rango de Edad': 'xx',
-                'Nivel Socioeconomico': 'xx',
-                'Region del Pais': 'xx',
-                'Antecedentes de Mora': 'xx',
-                'Hanito de consumo': 'xx',
-                'Tipo de Personalidad': 'xx',
-                'Responsabilidad': 'xx',
-                'Empatia': 'xx',
-                'Estabilidad Emocional': 'xx',
-                'Honestidad': 'xx',
-                'Nivel de Comunicacion': 'xx',
+                'clasificacion_tipo': 'responde solo con ALTO, MEDIO, BAJO',
+                'notificacion': 'responde solo con WHATSAPP, EMAIL, CARTA, LLAMADA',
+                'speech': 'SPEECH solicitando el pago de la mora SI ES QUE APLICA'
             })
         )},
         ]
     )
-    total.append({**json.loads(response.choices[0].message.content), **item})
-    print(total[-1])
+    res = response.choices[0].message.content
+    try:
+        total.append({**json.loads(res), **item})
+        print(total[-1])
+    except:
+        print(f'ERROR: {res}')
     time.sleep(.5)
 
-with open('response_prompt.json', 'w') as file:
+with open('response_prompt_2.json', 'w') as file:
     json.dump(total, file, indent=4)
